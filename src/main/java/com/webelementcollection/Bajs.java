74fef25f01bd297baa3f4551a57bd1bad11c4a5a
mangan <magnus.granander@etraveli.com>
2013-10-30 00:10:11 +0100

    Initial commit

---
+++ src/main/java/com/webelementcollection/Bajs.java
@@ -0,0 +1,10 @@
+package com.webelementcollection;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+/**
+ * <br> User: Mangan <br> Date: 29/10/13
+ */
+@ParametersAreNonnullByDefault
+public class Bajs {
+}
---
+++ src/main/java/com/webelementcollection/ElementCollection.java
@@ -0,0 +1,46 @@
+package com.webelementcollection;
+
+
+import java.util.List;
+
+/**
+ * <p/>
+ * <h5>Wrapper for Selenium WebElement</h5>
+ * <p/>
+ * <pre>
+ * User: Mangan
+ * Date: 6/29/12
+ * </pre>
+ */
+public interface ElementCollection extends ElementCollectionFinder {
+
+    ElementCollection click();
+
+    ElementCollection submit();
+
+    ElementCollection get(int index);
+
+    ElementCollection first();
+
+    ElementCollection last();
+
+    ElementCollection val(String value);
+
+    ElementCollection val(boolean value);
+
+    List<ElementCollection> getElements();
+
+    ElementCollection val(int value);
+
+    String val();
+
+    String attr(String name);
+
+    int length();
+
+    ElementCollection valByIndex(int index);
+
+    boolean isDisplayed();
+
+    ElementCollection valByVisibleText(String text);
+}
