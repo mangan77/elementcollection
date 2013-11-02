552729b326995fbafe8bd4c51e5e84b1e030c718
mangan <magnus.granander@etraveli.com>
2013-11-02 23:04:34 +0100

    Custom mock for WebElement instead of using Mockito for mocking. Decorator for checkable elements.

--- src/test/java/com/webelementcollection/ElementCollectionImplTest.java
+++ src/test/java/com/webelementcollection/ElementCollectionImplTest.java
@@ -1,15 +1,13 @@
 package com.webelementcollection;
 
-import com.google.common.collect.Maps;
-import org.openqa.selenium.*;
-import org.testng.Assert;
+import com.webelementcollection.mock.WebElementMock;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 import static org.testng.Assert.*;
 
 /**
@@ -21,10 +19,6 @@ public class ElementCollectionImplTest {
 //
 //    }
 //
-//    public void testClick() throws Exception {
-//
-//    }
-//
     @Test(expectedExceptions = IllegalStateException.class)
     public void Clicking_An_Empty_Collection_Should_Throw_Illegal_Argument_Exception() {
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver());
@@ -33,18 +27,14 @@ public class ElementCollectionImplTest {
     }
 
     public void When_More_Then_One_Element_In_Collection_All_Elements_Should_Get_Clicked() {
-        final WebElement webElementOne = new WebElementMockBuilder().build();
-        final WebElement webElementTwo = new WebElementMockBuilder().build();
+        final WebElementMock webElementOne = new WebElementMock();
+        final WebElementMock webElementTwo = new WebElementMock();
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
 
         elementCollection.click();
-        verify(webElementOne).click();
-        verify(webElementTwo).click();
+        assertTrue(webElementOne.wasClicked());
+        assertTrue(webElementTwo.wasClicked());
     }
-
-//    public void testSubmit() throws Exception {
-//
-//    }
 
     @Test(expectedExceptions = IllegalStateException.class)
     public void Submitting_An_Empty_Collection_Should_Throw_Illegal_State_Exception() {
@@ -54,18 +44,14 @@ public class ElementCollectionImplTest {
     }
 
     public void When_More_Then_One_Element_In_Collection_All_Elements_Should_Get_Submitted() {
-        final WebElement webElementOne = new WebElementMockBuilder().build();
-        final WebElement webElementTwo = new WebElementMockBuilder().build();
+        final WebElementMock webElementOne = new WebElementMock();
+        final WebElementMock webElementTwo = new WebElementMock();
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
 
         elementCollection.submit();
-        verify(webElementOne).submit();
-        verify(webElementTwo).submit();
+        assertTrue(webElementOne.wasSubmitted());
+        assertTrue(webElementTwo.wasSubmitted());
     }
-//
-//    public void testGet() throws Exception {
-//
-//    }
 
     @Test(expectedExceptions = IllegalStateException.class)
     public void Getting_An_Element_From_An_Empty_Collection_Should_Throw_Illegal_State_Exception() {
@@ -77,7 +63,7 @@ public class ElementCollectionImplTest {
 
     @Test(expectedExceptions = IllegalStateException.class)
     public void Specifying_An_Index_Outside_The_Collection_Should_Throw_Illegal_State_Exception() {
-        final WebElement webElementOne = new WebElementMockBuilder().build();
+        final WebElement webElementOne = new WebElementMock();
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne);
 
         elementCollection.get(1);
@@ -85,20 +71,14 @@ public class ElementCollectionImplTest {
     }
 
     public void Getting_An_Element_In_The_Collection_Should_Return_The_Correct_Element() {
-        final WebElement webElementOne = new WebElementMockBuilder().build();
-        final WebElement webElementTwo = new WebElementMockBuilder().withId("two").build();
-        final WebElement webElementThree = new WebElementMockBuilder().build();
+        final WebElement webElementOne = new WebElementMock();
+        final WebElement webElementTwo = new WebElementMock().withAttribute("id", "two");
+        final WebElement webElementThree = new WebElementMock();
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(),
                 webElementOne, webElementTwo, webElementThree);
 
         assertEquals(elementCollection.get(1).attr("id"), "two");
     }
-
-//
-//    public void testFirst() throws Exception {
-//
-//    }
-//
 
     @Test(expectedExceptions = IllegalStateException.class)
     public void Getting_The_First_Element_From_An_Empty_Collection_Should_Throw_Illegal_State_Exception() {
@@ -109,8 +89,8 @@ public class ElementCollectionImplTest {
     }
 
     public void Getting_The_First_Element_From_A_Collection_Should_Return_The_First_Element() {
-        final WebElement webElementOne = new WebElementMockBuilder().withId("one").build();
-        final WebElement webElementTwo = new WebElementMockBuilder().withId("two").build();
+        final WebElement webElementOne = new WebElementMock().withAttribute("id", "one");
+        final WebElement webElementTwo = new WebElementMock().withAttribute("id", "two");
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
 
         assertEquals(elementCollection.first().attr("id"), "one");
@@ -125,8 +105,8 @@ public class ElementCollectionImplTest {
     }
 
     public void Getting_The_Last_Element_From_A_Collection_Should_Return_The_Last_Element() {
-        final WebElement webElementOne = new WebElementMockBuilder().withId("one").build();
-        final WebElement webElementTwo = new WebElementMockBuilder().withId("two").build();
+        final WebElement webElementOne = new WebElementMock().withAttribute("id", "one");
+        final WebElement webElementTwo = new WebElementMock().withAttribute("id", "two");
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
 
         assertEquals(elementCollection.last().attr("id"), "two");
@@ -137,10 +117,6 @@ public class ElementCollectionImplTest {
 //
 //    }
 //
-//    public void testVal() throws Exception {
-//
-//    }
-
 
     public void Getting_Value_From_Empty_Collection_Should_Return_Null() {
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver());
@@ -149,41 +125,43 @@ public class ElementCollectionImplTest {
     }
 
     public void Getting_Value_From_A_Collection_Should_Return_The_Value_Of_The_First_Element() {
-        final WebElement webElementOne = new WebElementMockBuilder().withValue("one").build();
-        final WebElement webElementTwo = new WebElementMockBuilder().withValue("two").build();
+        final WebElement webElementOne = new WebElementMock();
+        webElementOne.sendKeys("one");
+        final WebElement webElementTwo = new WebElementMock();
+        webElementTwo.sendKeys("two");
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
 
         assertEquals(elementCollection.val(), "one");
     }
 
     public void Setting_Value_True_To_An_Unchecked_Checkable_Should_Make_It_Checked() {
-        final WebElement webElementOne = new WebElementMockBuilder().build();
+        final WebElement webElementOne = new WebElementMock().asCheckable();
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne);
 
         elementCollection.val(true);
-        verify(webElementOne).click();
+        assertTrue(webElementOne.isSelected());
     }
 
     public void Setting_Value_True_To_An_Already_Checked_Checkable_Should_Leave_It_Checked() {
-        final WebElement webElement = new WebElementMockBuilder().isSelected().build();
+        final WebElement webElement = new WebElementMock().setSelected(true);
 
         final ElementCollectionImpl elementCollection = new ElementCollectionImpl(mockWebDriver(), webElement);
 
         elementCollection.val(true);
-        verify(webElement, times(2)).isSelected(); // Once when creating the mock and once when checking it
+        assertTrue(webElement.isSelected());
     }
 
     public void testSettingValueFalseToACheckedCheckboxShouldMakeItUnchecked() {
-        Assert.assertTrue(false);
+        assertTrue(false);
     }
 
     public void testSettingValueFalseToAnAlreadyUncheckedCheckboxShouldLeaveItUnchecked() {
-        Assert.assertTrue(false);
+        assertTrue(false);
     }
 
     @Test
     public void testCheckingNonCheckableElementShouldThrowIllegalStateException() {
-        Assert.assertTrue(false);
+        assertTrue(false);
     }
 //
 //    public void testValByIndex() throws Exception {
@@ -218,10 +196,6 @@ public class ElementCollectionImplTest {
 //
 //    }
 //
-//    public void testGetElements() throws Exception {
-//
-//    }
-//
 
     public void No_Element_In_Collection_Should_Give_False() {
         final ElementCollection elementCollection = new ElementCollectionImpl(mockWebDriver(), null, Collections.EMPTY_LIST);
@@ -229,15 +203,15 @@ public class ElementCollectionImplTest {
     }
 
     public void When_At_Least_One_Element_In_Collection_Is_Not_Displayed_Should_Give_False() throws Exception {
-        final WebElement webElementOne = new WebElementMockBuilder().isDisplayed().build();
-        final WebElement webElementTwo = new WebElementMockBuilder().build();
+        final WebElement webElementOne = new WebElementMock().setDisplayed(true);
+        final WebElement webElementTwo = new WebElementMock();
         final ElementCollection elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
         assertFalse(elementCollection.isDisplayed());
     }
 
     public void When_All_Elements_Are_Displayed_In_Collection_Should_Give_True() throws Exception {
-        final WebElement webElementOne = new WebElementMockBuilder().isDisplayed().build();
-        final WebElement webElementTwo = new WebElementMockBuilder().isDisplayed().build();
+        final WebElement webElementOne = new WebElementMock().setDisplayed(true);
+        final WebElement webElementTwo = new WebElementMock().setDisplayed(true);
         final ElementCollection elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
         assertTrue(elementCollection.isDisplayed());
     }
@@ -249,8 +223,8 @@ public class ElementCollectionImplTest {
     }
 
     public void Length_Is_Same_As_Number_Of_Elements_In_Collection() throws Exception {
-        final WebElement webElementOne = new WebElementMockBuilder().build();
-        final WebElement webElementTwo = new WebElementMockBuilder().build();
+        final WebElement webElementOne = new WebElementMock();
+        final WebElement webElementTwo = new WebElementMock();
         final ElementCollection elementCollection = new ElementCollectionImpl(mockWebDriver(), webElementOne, webElementTwo);
 
         assertEquals(elementCollection.length(), 2);
@@ -259,129 +233,5 @@ public class ElementCollectionImplTest {
     private static WebDriver mockWebDriver() {
         return mock(WebDriver.class);
     }
-
-    private class WebElementMockBuilder {
-        private boolean isDisplayed;
-        private String id;
-        private final Map<String, String> attributes;
-        private String text;
-        private boolean isSelected;
-
-        private WebElementMockBuilder() {
-            attributes = Maps.newHashMap();
-        }
-
-        public WebElementMockBuilder isDisplayed() {
-            isDisplayed = true;
-            return this;
-        }
-
-        public WebElement build() {
-            final WebElement webElement = mock(WebElement.class);
-            // Is displayed
-            if (isDisplayed) {
-                when(webElement.isDisplayed()).thenReturn(true);
-            }
-            // Adding attributes
-            for (Map.Entry<String, String> attribute : attributes.entrySet()) {
-                when(webElement.getAttribute(attribute.getKey())).thenReturn(attribute.getValue());
-            }
-            // Set text
-            if (text != null) {
-                when(webElement.getText()).thenReturn(text);
-            }
-
-            when(webElement.isSelected()).thenReturn(isSelected);
-
-            return webElement;
-        }
-
-        public WebElementMockBuilder withId(String id) {
-            attributes.put("id", id);
-            return this;
-        }
-
-        public WebElementMockBuilder withValue(String value) {
-            text = value;
-            return this;
-        }
-
-        public WebElementMockBuilder isSelected() {
-            isSelected = true;
-            return this;
-        }
-    }
-
-    private class WebbElement implements WebElement {
-        @Override
-        public void click() {
-        }
-
-        @Override
-        public void submit() {
-        }
-
-        @Override
-        public void sendKeys(CharSequence... keysToSend) {
-        }
-
-        @Override
-        public void clear() {
-        }
-
-        @Override
-        public String getTagName() {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public String getAttribute(String name) {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public boolean isSelected() {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public boolean isEnabled() {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public String getText() {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public List<WebElement> findElements(By by) {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public WebElement findElement(By by) {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public boolean isDisplayed() {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public Point getLocation() {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public Dimension getSize() {
-            throw new RuntimeException("Not impl");
-        }
-
-        @Override
-        public String getCssValue(String propertyName) {
-            throw new RuntimeException("Not impl");
-        }
-    }
+//*
 }
